c083ced9e98d11712d6485fc341a1e8abb5bc8a6
Guilherme Rodrigues <157857247+GuilhermeRodOliveira@users.noreply.github.com>
2024-08-22 18:06:44 -0300

    Adicionar arquivos de projeto.

---
+++ ESTRUTURA LOOP DE REPETIÇÃO 6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Os primeiros 10 números da sequência de Fibonacci:");
+
+        int a = 0, b = 1;
+        int count = 0;
+        
+        //Colocar para o usuario digitar o limite de contagem
+
+        // Imprimir os primeiros 10 números da sequência de Fibonacci
+        while (count < 10)
+        {
+            Console.Write(a + " ");
+
+            int temporario = a;
+            a = b;
+            b = temporario + b;
+
+            count++;
+            
+        }
+
+        Console.WriteLine();
+    }
+}
